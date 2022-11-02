dfb1882896bd0b4adf9ef06abda586470f487c22
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-11-02 14:19:48 +0500

    [Hero-section] -Creted hero section of App Landing project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Hero from "./Hero/Hero";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,25 @@
+import screenImg from "../images/screens.svg";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1 className="hero-header">Work at the speed of thought</h1>
+        <p className="hero-header-description">
+          Most calendars are designed for teams. Slate is designed for
+          freelancers who want a simple way to plan their schedule.
+        </p>
+      </div>
+
+      <div className="hero-btns">
+        <button className="hero-btn left">Try For Free</button>
+        <button className="hero-btn right">Learn More</button>
+      </div>
+
+      <img className="hero-screen" src={screenImg}></img>
+    </div>
+  );
+};
+
+export default Hero;
