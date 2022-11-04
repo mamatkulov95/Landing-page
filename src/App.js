8df18935fef798fb7af440cc35e8f84e1ccee3ee
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-11-04 23:30:16 +0500

    [Content7] -Created content7 section of landing page project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Content7 from "./Content7/Content7";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Content7 />
+    </div>
+  );
 }
 
 export default App;
