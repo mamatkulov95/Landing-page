9c73ce4410bfb9aba8dc5a3466111db17ca8ef2d
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-11-20 08:28:37 +0500

    [Testimonials] -Created and developed testimonial section of landing page project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Testimonial from "./Components/Testimonials/Testimonial";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Testimonial />
+    </div>
+  );
 }
 
 export default App;
